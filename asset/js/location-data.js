84d11cb9112d68f9bd44f74e8ab3202ec995c18e
liyuening <liyuening@zhanshujia.com>
2018-04-11 00:07:04 +0800

    数据

---
+++ asset/js/location-data.js
@@ -0,0 +1,53 @@
+var site = {
+    "1": {
+        "image": "http://via.placeholder.com/400x300",
+        "openingHours": "9 a.m. – 4 p.m.",
+        "location": "澳门氹仔海边马路",
+        "name": "龙环葡韵",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, tenetur? Voluptatibus dolore neque libero,sapiente quo sequi, recusandae quod reprehenderit molestiae at architecto sunt quae laudantium consequatur tempore eaque porro? Lorem ipsum dolor sit amet consectetur, adipisicing elit.Vitae asperiores adipisci reiciendis maxime, tenetur modi in ad.Vitae omnis praesentium voluptatibus dicta pariatur quasi amet cum laborum!Quibusdam totam maxime, quam quia laborum natus! Ut odit dolores pariatur aperiam velit temporibus itaque quod ad error totam sapiente delectus rem fugit, optio quis eveniet animi nemo modi saepe nesciunt tenetur aliquam possimus sint.Earum architecto, reprehenderit molestias soluta mollitia eaque aliquam beatae quidem quod vel aperiam.Quibusdam tenetur eligendi pariatur facilis.Odit quaerat excepturi ad molestias, deserunt illo nostrum minima quae quis facilis assumenda quibusdam hic adipisci tenetur eius non quia? Aut quasi ab porro molestias.Illum minus recusandae sequi facilis dignissimos aperiam voluptatem necessitatibus cum quaerat aut ut saepe beatae, error, incidunt aliquid eos nisi rerum sunt dolorem distinctio sint quibusdam ? Maiores alias dolore veniam corporis magni vitae sapiente nesciunt illo libero ullam eos ipsam ipsum repellat quo, necessitatibus, tempore illum.Minima esse soluta commodi accusantium minus, sit voluptate! Ad quis voluptatibus perspiciatis ab porro placeat fugit sapiente, voluptatem sit magnam asperiores beatae.Dolore veritatis architecto nemo nostrum! Quasi odit, nesciunt soluta quos repudiandae quidem aliquam impedit porro totam corporis reprehenderit fugiat, commodi perspiciatis eaque obcaecati fuga, exercitationem id ipsa?",
+        "relatedLocations": [
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街"
+            },
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街2"
+            }
+        ]
+    },
+    "2": {
+        "image": "http://b3-q.mafengwo.net/s6/M00/11/72/wKgB4lMVcpmAQQlRAAGw8ilAe8M38.jpeg?im…nail%2F%211360x940r%2Fgravity%2FCenter%2Fcrop%2F%211360x940%2Fquality%2F90",
+        "openingHours": "9 a.m. – 4 p.m.",
+        "location": "澳门氹仔海边马路",
+        "name": "龙环葡韵2",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, tenetur? Voluptatibus dolore neque libero,sapiente quo sequi, recusandae quod reprehenderit molestiae at architecto sunt quae laudantium consequatur tempore eaque porro? Lorem ipsum dolor sit amet consectetur, adipisicing elit.Vitae asperiores adipisci reiciendis maxime, tenetur modi in ad.Vitae omnis praesentium voluptatibus dicta pariatur quasi amet cum laborum!Quibusdam totam maxime, quam quia laborum natus! Ut odit dolores pariatur aperiam velit temporibus itaque quod ad error totam sapiente delectus rem fugit, optio quis eveniet animi nemo modi saepe nesciunt tenetur aliquam possimus sint.Earum architecto, reprehenderit molestias soluta mollitia eaque aliquam beatae quidem quod vel aperiam.Quibusdam tenetur eligendi pariatur facilis.Odit quaerat excepturi ad molestias, deserunt illo nostrum minima quae quis facilis assumenda quibusdam hic adipisci tenetur eius non quia? Aut quasi ab porro molestias.Illum minus recusandae sequi facilis dignissimos aperiam voluptatem necessitatibus cum quaerat aut ut saepe beatae, error, incidunt aliquid eos nisi rerum sunt dolorem distinctio sint quibusdam ? Maiores alias dolore veniam corporis magni vitae sapiente nesciunt illo libero ullam eos ipsam ipsum repellat quo, necessitatibus, tempore illum.Minima esse soluta commodi accusantium minus, sit voluptate! Ad quis voluptatibus perspiciatis ab porro placeat fugit sapiente, voluptatem sit magnam asperiores beatae.Dolore veritatis architecto nemo nostrum! Quasi odit, nesciunt soluta quos repudiandae quidem aliquam impedit porro totam corporis reprehenderit fugiat, commodi perspiciatis eaque obcaecati fuga, exercitationem id ipsa?",
+        "relatedLocations": [
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街3"
+            },
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街4"
+            }
+        ]
+    },
+    "3": {
+        "image": "http://via.placeholder.com/400x303",
+        "openingHours": "9 a.m. – 4 p.m.",
+        "location": "澳门氹仔海边马路",
+        "name": "龙环葡韵3",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, tenetur? Voluptatibus dolore neque libero,sapiente quo sequi, recusandae quod reprehenderit molestiae at architecto sunt quae laudantium consequatur tempore eaque porro? Lorem ipsum dolor sit amet consectetur, adipisicing elit.Vitae asperiores adipisci reiciendis maxime, tenetur modi in ad.Vitae omnis praesentium voluptatibus dicta pariatur quasi amet cum laborum!Quibusdam totam maxime, quam quia laborum natus! Ut odit dolores pariatur aperiam velit temporibus itaque quod ad error totam sapiente delectus rem fugit, optio quis eveniet animi nemo modi saepe nesciunt tenetur aliquam possimus sint.Earum architecto, reprehenderit molestias soluta mollitia eaque aliquam beatae quidem quod vel aperiam.Quibusdam tenetur eligendi pariatur facilis.Odit quaerat excepturi ad molestias, deserunt illo nostrum minima quae quis facilis assumenda quibusdam hic adipisci tenetur eius non quia? Aut quasi ab porro molestias.Illum minus recusandae sequi facilis dignissimos aperiam voluptatem necessitatibus cum quaerat aut ut saepe beatae, error, incidunt aliquid eos nisi rerum sunt dolorem distinctio sint quibusdam ? Maiores alias dolore veniam corporis magni vitae sapiente nesciunt illo libero ullam eos ipsam ipsum repellat quo, necessitatibus, tempore illum.Minima esse soluta commodi accusantium minus, sit voluptate! Ad quis voluptatibus perspiciatis ab porro placeat fugit sapiente, voluptatem sit magnam asperiores beatae.Dolore veritatis architecto nemo nostrum! Quasi odit, nesciunt soluta quos repudiandae quidem aliquam impedit porro totam corporis reprehenderit fugiat, commodi perspiciatis eaque obcaecati fuga, exercitationem id ipsa?",
+        "relatedLocations": [
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街5"
+            },
+            {
+                "image": "http://via.placeholder.com/400x300",
+                "name": "管也街6"
+            }
+        ]
+    }
+};
