fba092cd607a470db79e1ca13ad91e4b32e7560f
liyuening <liyuening@zhanshujia.com>
2018-04-19 10:03:46 +0800

    1. feature / layer 2. minor fix

--- asset/js/main.js
+++ asset/js/main.js
@@ -127,6 +127,15 @@ app.RotateNorthControl = function (opt_options) {
     filterAll.addEventListener('click', handleRotateNorth, false);
     filterAll.addEventListener('touchstart', handleRotateNorth, false);
 
+    // TODO
+    function changeFilterViewSpot() {
+        this_.getMap().setLayerGroup(new ol.layer.Group({
+            layers: [picLayer, vectorLayer2]
+        }));
+    }
+    filterViewSpot.addEventListener('click', changeFilterViewSpot, false);
+    filterViewSpot.addEventListener('touchstart', changeFilterViewSpot, false);
+
     var element = document.createElement('div');
     element.className = 'ol-feature-filter ol-unselectable ol-control bs20 oh';
     element.appendChild(filterAll);
@@ -149,11 +158,108 @@ var zoomOut = document.createElement("img");
 zoomOut.setAttribute('src', 'asset/img/zoom-out-2.svg');
 zoomOut.setAttribute('alt', 'zoom-out');
 
+// layer1, feature1, feature2
+var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point([1200, 700]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
+        anchor: [0.5, 48],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+    }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+var iconFeature2 = new ol.Feature({
+    geometry: new ol.geom.Point([1200, 800]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle2 = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
+        anchor: [0.5, 48],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+    }))
+});
+
+iconFeature2.setStyle(iconStyle2);
+
+var vectorSource = new ol.source.Vector({
+    features: [iconFeature, iconFeature2]
+});
+
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+});
+
+// layer2, feature3, feature4
+var iconFeature3 = new ol.Feature({
+    geometry: new ol.geom.Point([1100, 700]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle3 = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
+        anchor: [0.5, 48],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+    }))
+});
+
+iconFeature3.setStyle(iconStyle3);
+
+var iconFeature4 = new ol.Feature({
+    geometry: new ol.geom.Point([1100, 800]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle4 = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
+        anchor: [0.5, 48],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+    }))
+});
+
+iconFeature4.setStyle(iconStyle4);
+
+var vectorSource2 = new ol.source.Vector({
+    features: [iconFeature3, iconFeature4]
+});
+
+var vectorLayer2 = new ol.layer.Vector({
+    source: vectorSource2
+});
+
 var extent = [0, 0, 2574, 1416];
 var projection = new ol.proj.Projection({
     code: 'macao-001',
     units: 'pixels',
     extent: extent
+});
+var picLayer = new ol.layer.Image({
+    source: new ol.source.ImageStatic({
+        url: 'https://ws1.sinaimg.cn/large/9130c6a9gy1fq9zf1bhwnj21zi13chdt.jpg',
+        projection: projection,
+        imageExtent: extent
+    })
 });
 var map = new ol.Map({
     controls: ol.control.defaults({
@@ -163,17 +269,8 @@ var map = new ol.Map({
             zoomOutLabel: zoomOut
         }
     }).extend([new app.RotateNorthControl()]),
-    target: 'map',
-    layers: [
-        new ol.layer.Image({
-            source: new ol.source.ImageStatic({
-                // attributions: '© <a href="http://xkcd.com/license.html">xkcd</a>',
-                url: 'https://ws1.sinaimg.cn/large/9130c6a9gy1fq9zf1bhwnj21zi13chdt.jpg',
-                projection: projection,
-                imageExtent: extent
-            })
-        })
-    ],
+    target: document.getElementById('map'),
+    layers: [picLayer, vectorLayer, vectorLayer2],
     view: new ol.View({
         projection: projection,
         center: ol.extent.getCenter(extent),
@@ -184,4 +281,45 @@ var map = new ol.Map({
         // zoom: 2,
         // maxZoom: 8
     })
+});
+
+var element = document.getElementById('popup');
+
+var popup = new ol.Overlay({
+    element: element,
+    positioning: 'bottom-center',
+    stopEvent: false,
+    offset: [0, -50]
+});
+map.addOverlay(popup);
+
+// display popup on click
+map.on('click', function (evt) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+        function (feature) {
+            return feature;
+        });
+    if (feature) {
+        var coordinates = feature.getGeometry().getCoordinates();
+        popup.setPosition(coordinates);
+        // $(element).popover({
+        //     'placement': 'top',
+        //     'html': true,
+        //     'content': feature.get('name')
+        // });
+        // $(element).popover('show');
+    } else {
+        // $(element).popover('destroy');
+    }
+});
+
+// change mouse cursor when over marker
+map.on('pointermove', function (e) {
+    if (e.dragging) {
+        // $(element).popover('destroy');
+        return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
 });
