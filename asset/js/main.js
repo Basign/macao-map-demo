0906de1fa66785f1247764d8bf6baab8cea4d064
liyuening <liyuening@zhanshujia.com>
2018-07-19 09:37:13 +0800

    attempt to sniff IE10

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,35 @@
 // IE10 特殊样式
-var isIE = function (ver) {
-    var b = document.createElement('b');
-    b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->';
-    return b.getElementsByTagName('i').length === 1;
-}
-if (!isIE(10)) {
+var IE = (function () {
+    "use strict";
+
+    var ret, isTheBrowser,
+        actualVersion,
+        jscriptMap, jscriptVersion;
+
+    isTheBrowser = false;
+    jscriptMap = {
+        "5.5": "5.5",
+        "5.6": "6",
+        "5.7": "7",
+        "5.8": "8",
+        "9": "9",
+        "10": "10"
+    };
+    jscriptVersion = new Function("/*@cc_on return @_jscript_version; @*/")();
+
+    if (jscriptVersion !== undefined) {
+        isTheBrowser = true;
+        actualVersion = jscriptMap[jscriptVersion];
+    }
+
+    ret = {
+        isTheBrowser: isTheBrowser,
+        actualVersion: actualVersion
+    };
+
+    return ret;
+}());
+if (IE.actualVersion !== 10) {
     document.body.innerHTML += "<style>body{overflow:hidden;}</style>";
 }
 
